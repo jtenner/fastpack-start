7976037b76318803bb7170907a17c4e0346d26f2
Joshua Tenner <tenner.joshua@gmail.com>
2019-05-09 16:56:31 -0400

    update example to obtain the web assembly module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 // @ts-ignore: import wasm
 import buffer from "../build/untouched.wasm";
-
-enum anEnum {
-  ONE,
-  TWO
-}
-
-console.log(buffer);
+import { instantiateBuffer } from "assemblyscript/lib/loader";
+const bin = Buffer.from(buffer.slice(29), "base64");
+const asutil = instantiateBuffer(bin);
+console.log(asutil);
